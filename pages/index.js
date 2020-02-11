345d999d94f4984067f26f2a300a53838fb9e5df
luml <1520622848@qq.com>
2020-02-11 11:40:11 +0800

    add router

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
 
+
+const PostLink = props => (
+    <li>
+        <Link href="/p/[id]" as={`/p/${props.id}`}>
+            <a>{props.id}</a>
+        </Link>
+    </li>
+)
+
 export default function Index() {
     return (
         <Layout>
-            <p>Hello Next.js</p>
+            <h1>My Blogs</h1>
             <ul>
-                <li>
-                    <Link href="/blog/[slug]" as="/blog/hello-world">
-                        <a>To hello World Blog post</a>
-                    </Link>
-                </li>
+                <PostLink id="Hello-Next.js" />
+                <PostLink id="Learn-Next.js-is-awesome" />
+                <PostLink id="Deploy-apps-with-Zeit" />
             </ul>
         </Layout>
     )
